ebac4dbf2d6213fe364dae9317af34796416e1f7
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2021-04-25 09:23:08 +0530

    update: moving init to app
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- app/app.go
+++ app/app.go
@@ -4,11 +4,26 @@ import (
 	"backend/app/auth"
 	_ "backend/app/auth/router"
 
+	"github.com/beego/beego/v2/client/orm"
 	beego "github.com/beego/beego/v2/server/web"
 	"github.com/otiai10/copy"
+	"gopkg.in/hlandau/passlib.v1"
 )
 
 func InitBeego() {
+	sslmode := "require&sslrootcert=conf/ca.crt&sslkey=conf/client.root.key&sslcert=conf/client.root.crt"
+	if beego.BConfig.RunMode == "dev" {
+		sslmode = "disable"
+	}
+	dbdriver := beego.AppConfig.DefaultString("dbdriver", "")
+	conn := "postgresql://" +
+		beego.AppConfig.DefaultString(dbdriver+"_user", "") + ":" +
+		beego.AppConfig.DefaultString(dbdriver+"_pass", "") + "@" +
+		beego.AppConfig.DefaultString(dbdriver+"_urls", "") + "/" +
+		beego.AppConfig.DefaultString(dbdriver+"_db", "") + "?sslmode=" + sslmode
+
+	_ = orm.RegisterDataBase("default", dbdriver, conn)
+	_ = passlib.UseDefaults(passlib.DefaultsLatest)
 	_ = beego.AddFuncMap("hasPermission", auth.HasPermission)
 	_ = beego.AddFuncMap("hasRole", auth.HasRole)
 	err := copy.Copy("./app/views/", "./"+beego.BConfig.WebConfig.ViewsPath+"/honeypot/")
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"backend/app"
 	_ "backend/routers"
 
@@ -10,27 +8,8 @@ import (
 
 	"github.com/beego/beego/v2/client/orm"
 	beego "github.com/beego/beego/v2/server/web"
-	"gopkg.in/hlandau/passlib.v1"
 )
 
-func init() {
-	// _ = os.Setenv("TZ", "America/Toronto")
-	sslmode := "require&sslrootcert=conf/ca.crt&sslkey=conf/client.root.key&sslcert=conf/client.root.crt"
-	if beego.BConfig.RunMode == "dev" {
-		sslmode = "disable"
-	}
-	dbdriver := beego.AppConfig.DefaultString("dbdriver", "")
-	conn := "postgresql://" +
-		beego.AppConfig.DefaultString(dbdriver+"_user", "") + ":" +
-		beego.AppConfig.DefaultString(dbdriver+"_pass", "") + "@" +
-		beego.AppConfig.DefaultString(dbdriver+"_urls", "") + "/" +
-		beego.AppConfig.DefaultString(dbdriver+"_db", "") + "?sslmode=" + sslmode
-	fmt.Println(dbdriver)
-	fmt.Println(conn)
-
-	_ = orm.RegisterDataBase("default", dbdriver, conn)
-	_ = passlib.UseDefaults(passlib.DefaultsLatest)
-}
 func main() {
 	orm.Debug = true
 	app.InitBeego()
